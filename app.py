2a4a40eb1e89d4285d49d03bf0b659e1f402717d
Brenton Bongcaron <brenton3926@gmail.com>
2021-01-16 01:57:26 -0500

    Implemented SQLite3 database in place of Excel spreadsheet

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
+from numpy import *
 import pandas as pd
+import sqlite3
+# SQLite3 automatically creates the PRIMARY_KEY
 
 def removeDuplicates(og_list):
     list = []
@@ -17,23 +20,24 @@ def removeDuplicates(og_list):
         i += 1
     return list
 
-def getPossibleStates(components, states, target):
-    possibleStates = []
-    for index, c in enumerate(components):
-        if c == target:
-            if str.lower(states[index]) == 'solid':
-                possibleStates.append(0)
-            elif str.lower(states[index]) == 'liquid':
-                possibleStates.append(1)
-            elif str.lower(states[index]) == 'gas':
-                possibleStates.append(2)
-            else:
-                raise Exception(f'Dataframe error. {states[index]} is an invalid state.')
-    return possibleStates
+def getPossibleStates(target):
+    possibleStates = query("SELECT state FROM coefficients WHERE component_name = '" + target + "'")
+    result = []
+    for state in possibleStates:
+        if str.lower(state) == 'solid':
+            result.append(0)
+        elif str.lower(state) == 'liquid':
+            result.append(1)
+        elif str.lower(state) == 'gas':
+            result.append(2)
+        else:
+            raise Exception(f'Dataframe error. {state} is an invalid state.')
+    return result
 
-def main():
+def buildDatabase():
     # Import Excel spreadsheet as dataframe
     heatCapacity_dataframe = pd.read_excel("heatCapacity.xlsx")
+    numRows = heatCapacity_dataframe.shape[0]
     # List of all components, including duplicates
     all_components = heatCapacity_dataframe['Component'].tolist()
     # Create a non-duplicate components list (default OptionMenu list @index 0)
@@ -41,21 +45,83 @@ def main():
     # Create a component state list corresponding to each row in the original dataframe
     component_states = heatCapacity_dataframe['State'].tolist()
 
+    # Creating/connecting to a databse for heat capacity coefficients
+    heatCapacity_db = sqlite3.connect('heat_capacity.db')
+    # Creating the cursor: little "robot" you send off to do actions
+    c = heatCapacity_db.cursor()
+    # Create table
+    c.execute("""CREATE TABLE coefficients (
+            component_name text,
+            state text,
+           a real,
+            b real,
+            c real,
+            d real,
+            temp_unit text,
+            low_temp real,
+            high_temp real
+            )""")
+    for i in range(numRows):
+        component_row = heatCapacity_dataframe.loc[i]
+        component_a = 0
+        component_b = 0
+        component_c = 0
+        component_d = 0
+        if type(component_row['a']) != str: component_a = float(component_row['a'])
+        if type(component_row['b']) != str: component_b = float(component_row['b'])
+        if type(component_row['c']) != str: component_c = float(component_row['c'])
+        if type(component_row['d']) != str: component_d = float(component_row['d'])
+        c.execute("INSERT INTO coefficients VALUES (:component_name, :state, :a, :b, :c, :d, :temp_unit, :low_temp, :high_temp)",
+            {
+                'component_name': component_row["Component"],
+                'state': component_row["State"],
+                'a': component_a,
+                'b': component_b,
+                'c': component_c,
+                'd': component_d,
+                'temp_unit': component_row["T Unit"],
+                'low_temp': float(component_row["Low T"]),
+                'high_temp': float(component_row["High T"])
+            }
+        )
+    #Commit Changes
+    heatCapacity_db.commit()
+    # Close database connection
+    c.close()
+
+def query(cmd):
+    heatCapacity_db = sqlite3.connect('heat_capacity.db')
+    c = heatCapacity_db.cursor()
+    # * means everything, oid is the primary key
+    c.execute(cmd)
+    data = c.fetchall()
+    result = []
+    for element in data:
+        result.append(element[0])
+    return result
+
+def main():
+    #buildDatabase()
+    component_options = removeDuplicates(query("SELECT component_name FROM coefficients"))
+
     root = Tk()
+
     # Creating Title Widget
     title = Label(root, text="Enthalpy Calculator - Heat of Capacity")
     title.grid(row=0, columnspan=5)
+
     # Creating the variables storing component names
     component1 = StringVar(root)
-    component1.set(component_options[0])
+    component1.set("Select a component")
     component2 = StringVar(root)
-    component2.set(component_options[0])
+    component2.set("Select a component")
 
     # Creating component1 OptionMenu
     component1_prompt = Label(root, text="Select component 1:")
     component1_menu = OptionMenu(root, component1, *component_options)
     component1_prompt.grid(row=1, column=0)
     component1_menu.grid(row=1, column=1)
+
     # Creating component2 OptionMenu
     component2_prompt = Label(root, text="Select component 2:")
     component2_menu = OptionMenu(root, component2, *component_options)
@@ -67,6 +133,7 @@ def main():
     state2 = IntVar(root)
     state_buttons1 = []
     state_buttons2 = []
+
     # Create all possible state Radiobuttons
     state_buttons1.append(Radiobutton(root, text="solid", variable=state1, value="0"))
     state_buttons1.append(Radiobutton(root, text="liquid", variable=state1, value="1"))
@@ -84,11 +151,13 @@ def main():
 
     def createStatesForm():
         # Get a list of all possible states components 1 and 2 can be in
-        possible_states1 = getPossibleStates(all_components, component_states, component1.get())
-        possible_states2 = getPossibleStates(all_components, component_states, component2.get())
+        possible_states1 = getPossibleStates(component1.get())
+        possible_states2 = getPossibleStates(component2.get())
+
         # Set the default state for each Radiobutton list
         state1.set(possible_states1[0])
         state2.set(possible_states2[0])
+
         # Component 1 state wdigets
         currentRow = 2
         state1_prompt = Label(root, text=f'Select the state for {component1.get()}:')
@@ -96,6 +165,7 @@ def main():
         for i in range(len(possible_states1)):
             state_buttons1[possible_states1[i]].grid(row=currentRow,column=1)
             currentRow += 1
+        
         # Component 2 state widgets
         currentRow = 2
         state2_prompt = Label(root, text=f'Select the state for {component2.get()}:')
@@ -103,13 +173,11 @@ def main():
         for i in range(len(possible_states2)):
             state_buttons2[possible_states2[i]].grid(row=currentRow, column=4)
             currentRow += 1
+        
         # Grid submit component states button
         submitStates_button.grid(row=max(len(possible_states1),len(possible_states2))+2, column=2)
 
     def submitComponents():
-        if component1.get() == 'Select a component' or component2.get() == 'Select a component':
-            messagebox.showerror(title="Error", message="One or more components have not been selected. Try again.")
-            return
         component1_menu.destroy()
         component2_menu.destroy()
         submitComponents_button.destroy()
