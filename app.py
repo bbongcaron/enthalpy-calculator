186b44d4801cde5856fee9d55310eb908bc8e6ad
Brenton Bongcaron <brenton3926@gmail.com>
2021-01-15 22:11:08 -0500

    Completed basic component OptionMenus and selection confirmation

--- app.py
+++ app.py
@@ -21,6 +21,46 @@ def main():
     # Create a component state list corresponding to each row in the original dataframe
     component_states = heatCapacity_dataframe['State'].tolist()
 
+    root = Tk()
+    component1 = 'none'
+    component2 = 'none'
+    # Creating Title Widget
+    title = Label(root, text="Temperature vs. Mole Fraction Phase Diagram Generator")
+    title.grid(row=0)
+    # Creating the variables storing component names
+    c1 = StringVar(root)
+    c1.set(component_options[0])
+    c2 = StringVar(root)
+    c2.set(component_options[0])
+        
+    # Creating component1 OptionMenu
+    component1_prompt = Label(root, text="Select component 1:")
+    component1_menu = OptionMenu(root, c1, *component_options)
+    component1_prompt.grid(row=2, column=0)
+    component1_menu.grid(row=2,column=1)
+    # Creating component2 OptionMenu
+    component2_prompt = Label(root, text="Select component 2:")
+    component2_menu = OptionMenu(root, c2, *component_options)
+    component2_prompt.grid(row=3, column=0)
+    component2_menu.grid(row=3,column=1)
+
+    
+    def submitComponents():
+        component1 = c1.get()
+        component2 = c2.get()
+        component1_menu.destroy()
+        component2_menu.destroy()
+        submitComponents_button.destroy()
+        component1_label = Label(root, text=component1)
+        component2_label = Label(root, text=component2)
+        component1_label.grid(row=2,column=1)
+        component2_label.grid(row=3,column=1)
+    
+    # Submit component values button
+    submitComponents_button = Button(root, text="Confirm Components", command=submitComponents)
+    submitComponents_button.grid(row=4)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
